aaf896b3e8f69ef2ed718499ec85dce858cf58b4
Shaund12 <Shaund234@gmail.com>
2026-01-12 18:45:05 -0500

    Add watch import to LauncherCreateView.vue
    Imported the 'watch' function from Vue in LauncherCreateView.vue, likely to enable reactive watching of state or props within the component.

--- src/views/LauncherCreateView.vue
+++ src/views/LauncherCreateView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, ref } from "vue";
+import { computed, ref, watch } from "vue";
 import { useRouter } from "vue-router";
 import RcDisclaimer from "@/components/RcDisclaimer.vue";
 import RcLoadingSpinner from "@/components/RcLoadingSpinner.vue";
